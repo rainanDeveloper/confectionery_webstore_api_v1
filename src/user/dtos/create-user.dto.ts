6dc6e067d9cc7b95be72eb7bfecaf4877ea91bbb
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2023-06-08 10:21:02 -0300

    Added the api body props on the create and update dtos

--- src/user/dtos/create-user.dto.ts
+++ src/user/dtos/create-user.dto.ts
@@ -1,16 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, IsEmail, MinLength } from 'class-validator';
 
 export class CreateUserDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({ example: 'john_doe' })
   login: string;
 
   @IsNotEmpty()
   @IsEmail()
+  @ApiProperty({ example: 'john.doe@email.com' })
   email: string;
 
   @IsNotEmpty()
   @IsString()
   @MinLength(8)
+  @ApiProperty({ example: '6waKSyOn3R' })
   password: string;
 }
--- src/user/dtos/update-user.dto.ts
+++ src/user/dtos/update-user.dto.ts
@@ -1,15 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsEmail, IsOptional, IsString } from 'class-validator';
 
 export class UpdateUserDto {
   @IsString()
   @IsOptional()
+  @ApiProperty({ example: 'john_doe' })
   login?: string;
 
   @IsEmail()
   @IsOptional()
+  @ApiProperty({ example: 'john.doe@email.com' })
   email?: string;
 
   @IsBoolean()
   @IsOptional()
+  @ApiProperty({ example: '6waKSyOn3R' })
   isActive?: boolean;
 }
