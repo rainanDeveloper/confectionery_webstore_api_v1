6e8c82e43331c82db518eed37d1de3c4bbe80f6c
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2023-08-14 21:36:28 -0300

    feat: Added endpoint for creating a product

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Inject, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiTags } from '@nestjs/swagger';
 import { CreateProductDto } from './dtos/create-product.dto';
 import { ProductEntity } from './entities/product.entity';
 import { ProductService } from './product.service';
 
+@ApiTags('Products')
 @Controller('product')
 export class ProductController {
   constructor(
