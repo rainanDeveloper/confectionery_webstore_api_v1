b09fb88241bd6e4749772ec4fc85c6f04c3e37b5
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2023-08-14 21:31:35 -0300

    feat: Implemented create product on product controller

--- src/product/product.controller.spec.ts
+++ src/product/product.controller.spec.ts
@@ -1,18 +1,52 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateProductDto } from './dtos/create-product.dto';
+import { ProductEntity } from './entities/product.entity';
 import { ProductController } from './product.controller';
+import { ProductService } from './product.service';
 
 describe('ProductController', () => {
-  let controller: ProductController;
+  let productController: ProductController;
+  let productService: ProductService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductController],
+      providers: [
+        {
+          provide: ProductService,
+          useValue: {
+            create: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<ProductController>(ProductController);
+    productController = module.get<ProductController>(ProductController);
+    productService = module.get<ProductService>(ProductService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(productController).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a product successfully', async () => {
+      const productMock = new ProductEntity();
+
+      const productDto: CreateProductDto = {
+        title: 'some product',
+        description: 'some description',
+        cost: 25,
+        unitValue: 30,
+      };
+
+      jest.spyOn(productService, 'create').mockResolvedValueOnce(productMock);
+
+      const result = await productController.create(productDto);
+
+      expect(result).toStrictEqual(productMock);
+      expect(productService.create).toHaveBeenCalledTimes(1);
+      expect(productService.create).toHaveBeenCalledWith(productDto);
+    });
   });
 });
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Inject, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { CreateProductDto } from './dtos/create-product.dto';
+import { ProductEntity } from './entities/product.entity';
+import { ProductService } from './product.service';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+  constructor(
+    @Inject(ProductService) private readonly productService: ProductService,
+  ) {}
+
+  @Post()
+  @UseGuards(AuthGuard('jwt'))
+  async create(productDto: CreateProductDto): Promise<ProductEntity> {
+    return this.productService.create(productDto);
+  }
+}
