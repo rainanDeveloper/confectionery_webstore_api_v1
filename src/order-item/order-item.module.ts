b0a4e01b82e1787b1d96b7721d6e52a9b64282b7
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2023-09-20 23:43:44 -0300

    fix: fixed typeorm to add new entities

--- src/order-item/order-item.module.ts
+++ src/order-item/order-item.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OrderItemEntity } from './entities/order-item.entity';
 
-@Module({})
+@Module({
+  imports: [ConfigModule, TypeOrmModule.forFeature([OrderItemEntity])],
+})
 export class OrderItemModule {}
