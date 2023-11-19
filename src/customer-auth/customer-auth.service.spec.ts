ff532e47248ee1054334dab4e780a2242491bbc4
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2023-11-19 16:29:15 -0300

    feat: Added validateCustomer method on customer-auth service

--- src/customer-auth/customer-auth.service.spec.ts
+++ src/customer-auth/customer-auth.service.spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { CustomerAuthService } from './customer-auth.service';
 import { CustomerService } from 'src/customer/customer.service';
 import { JwtService } from '@nestjs/jwt';
+import { CustomerEntity } from 'src/customer/entities/customer.entity';
+import { hashSync } from 'bcrypt';
 
 describe('CustomerAuthService', () => {
   let customerAuthService: CustomerAuthService;
@@ -15,7 +17,7 @@ describe('CustomerAuthService', () => {
         {
           provide: CustomerService,
           useValue: {
-            findOneByEmailOrLogin: jest.fn(),
+            findOneByLoginOrEmail: jest.fn(),
           },
         },
         {
@@ -36,5 +38,89 @@ describe('CustomerAuthService', () => {
     expect(customerAuthService).toBeDefined();
     expect(customerService).toBeDefined();
     expect(jwtService).toBeDefined();
+  });
+
+  describe('validateCustomer', () => {
+    it('should validate a customer successfully', async () => {
+      const login = 'some_login';
+      const password = 'cbvaY2CEmXaqQpin';
+
+      const customerMock: CustomerEntity = new CustomerEntity();
+
+      customerMock.login = login;
+      customerMock.password = hashSync(password, 8);
+
+      jest
+        .spyOn(customerService, 'findOneByLoginOrEmail')
+        .mockResolvedValueOnce(customerMock);
+
+      const result = await customerAuthService.validateCustomer(
+        login,
+        password,
+      );
+
+      expect(result).toStrictEqual(customerMock);
+      expect(customerService.findOneByLoginOrEmail).toHaveBeenCalledTimes(1);
+      expect(customerService.findOneByLoginOrEmail).toHaveBeenCalledWith(login);
+    });
+
+    it('should return null if a customer is not found', async () => {
+      const login = 'some_login';
+      const password = 'cbvaY2CEmXaqQpin';
+
+      jest
+        .spyOn(customerService, 'findOneByLoginOrEmail')
+        .mockResolvedValueOnce(undefined);
+
+      const result = await customerAuthService.validateCustomer(
+        login,
+        password,
+      );
+
+      expect(result).toStrictEqual(null);
+      expect(customerService.findOneByLoginOrEmail).toHaveBeenCalledTimes(1);
+      expect(customerService.findOneByLoginOrEmail).toHaveBeenCalledWith(login);
+    });
+
+    it('should return null if method to return customer fails', async () => {
+      const login = 'some_login';
+      const password = 'cbvaY2CEmXaqQpin';
+
+      jest
+        .spyOn(customerService, 'findOneByLoginOrEmail')
+        .mockRejectedValueOnce(new Error());
+
+      const result = await customerAuthService.validateCustomer(
+        login,
+        password,
+      );
+
+      expect(result).toStrictEqual(null);
+      expect(customerService.findOneByLoginOrEmail).toHaveBeenCalledTimes(1);
+      expect(customerService.findOneByLoginOrEmail).toHaveBeenCalledWith(login);
+    });
+
+    it('should return null if method to return customer fails', async () => {
+      const login = 'some_login';
+      const password = 'cbvaY2CEmXaqQpin';
+
+      const customerMock: CustomerEntity = new CustomerEntity();
+
+      customerMock.login = login;
+      customerMock.password = hashSync('anotherPass', 8);
+
+      jest
+        .spyOn(customerService, 'findOneByLoginOrEmail')
+        .mockResolvedValueOnce(customerMock);
+
+      const result = await customerAuthService.validateCustomer(
+        login,
+        password,
+      );
+
+      expect(result).toStrictEqual(null);
+      expect(customerService.findOneByLoginOrEmail).toHaveBeenCalledTimes(1);
+      expect(customerService.findOneByLoginOrEmail).toHaveBeenCalledWith(login);
+    });
   });
 });
--- src/customer-auth/customer-auth.service.ts
+++ src/customer-auth/customer-auth.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { compareSync } from 'bcrypt';
+import { CustomerService } from 'src/customer/customer.service';
 
 @Injectable()
-export class CustomerAuthService {}
+export class CustomerAuthService {
+  constructor(
+    @Inject(CustomerService) private readonly customerService: CustomerService,
+    @Inject(JwtService) private readonly jwtService: JwtService,
+  ) {}
+
+  async validateCustomer(login: string, password) {
+    let customer;
+
+    try {
+      customer = await this.customerService.findOneByLoginOrEmail(login);
+    } catch (error) {
+      return null;
+    }
+
+    if (!customer) return null;
+
+    const passwordIsValid = compareSync(password, customer.password);
+
+    if (!passwordIsValid) return null;
+
+    return customer;
+  }
+}
