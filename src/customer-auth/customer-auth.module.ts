9277861b1efbfe603074a5e2d935d6175f5cad13
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2023-11-19 17:01:04 -0300

    feat: Added dependencies to customer-auth module

--- src/customer-auth/customer-auth.module.ts
+++ src/customer-auth/customer-auth.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
 import { CustomerAuthService } from './customer-auth.service';
+import { CustomerModule } from 'src/customer/customer.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  providers: [CustomerAuthService]
+  imports: [
+    CustomerModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          secret: configService.getOrThrow('JWT_SECRET'),
+          signOptions: {
+            expiresIn: configService.get('JWT_EXPIRESIN') || '2h',
+          },
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [CustomerAuthService],
 })
 export class CustomerAuthModule {}
