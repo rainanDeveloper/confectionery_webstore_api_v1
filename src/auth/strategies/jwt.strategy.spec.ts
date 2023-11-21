cfa15bf34f7c0c03111f3fd8fd0f009585074354
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2023-11-21 15:11:47 -0300

    feat: Added validation of users on jwt strategy

--- src/auth/strategies/jwt.strategy.spec.ts
+++ src/auth/strategies/jwt.strategy.spec.ts
@@ -3,10 +3,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { randomBytes, randomUUID } from 'crypto';
 import { JwtPayloadDto } from '../dtos/jwt-payload.dto';
 import { JwtStrategy } from './jwt.strategy';
+import { UserService } from 'src/user/user.service';
+import { UserEntity } from 'src/user/entities/user.entity';
+import { UnauthorizedException } from '@nestjs/common';
 
 describe('JwtStrategy', () => {
   let jwtStrategy: JwtStrategy;
   let configService: ConfigService;
+  let userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -20,16 +24,24 @@ describe('JwtStrategy', () => {
               .mockReturnValueOnce(randomBytes(32).toString('hex')),
           },
         },
+        {
+          provide: UserService,
+          useValue: {
+            findOneById: jest.fn(),
+          },
+        },
       ],
     }).compile();
 
     jwtStrategy = module.get<JwtStrategy>(JwtStrategy);
     configService = module.get<ConfigService>(ConfigService);
+    userService = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
     expect(jwtStrategy).toBeDefined();
     expect(configService).toBeDefined();
+    expect(userService).toBeDefined();
   });
 
   describe('validate', () => {
@@ -39,6 +51,16 @@ describe('JwtStrategy', () => {
         login: 'some_login',
         email: 'some@login.mail',
       };
+
+      const userEntityMock: UserEntity = {
+        id: payloadMock.sub,
+        login: payloadMock.login,
+        email: payloadMock.email,
+      } as any;
+
+      jest
+        .spyOn(userService, 'findOneById')
+        .mockResolvedValueOnce(userEntityMock);
 
       const userMock = {
         id: payloadMock.sub,
@@ -52,6 +74,77 @@ describe('JwtStrategy', () => {
       expect(result).toEqual(userMock);
       expect(configService.getOrThrow).toHaveBeenCalledTimes(1);
       expect(configService.getOrThrow).toHaveBeenCalledWith('JWT_SECRET');
+      expect(userService.findOneById).toHaveBeenCalledTimes(1);
+      expect(userService.findOneById).toHaveBeenCalledWith(payloadMock.sub);
+    });
+    it('should throw a unauthorized exception', async () => {
+      const payloadMock: JwtPayloadDto = {
+        sub: randomUUID(),
+        login: 'some_login',
+        email: 'some@login.mail',
+      };
+
+      jest.spyOn(userService, 'findOneById').mockResolvedValueOnce(undefined);
+
+      const resultPromise = jwtStrategy.validate(payloadMock);
+
+      expect(resultPromise).rejects.toThrowError(UnauthorizedException);
+      expect(configService.getOrThrow).toHaveBeenCalledTimes(1);
+      expect(configService.getOrThrow).toHaveBeenCalledWith('JWT_SECRET');
+      expect(userService.findOneById).toHaveBeenCalledTimes(1);
+      expect(userService.findOneById).toHaveBeenCalledWith(payloadMock.sub);
+    });
+
+    it('should throw a unauthorized exception', async () => {
+      const payloadMock: JwtPayloadDto = {
+        sub: randomUUID(),
+        login: 'some_login',
+        email: 'some@login.mail',
+      };
+
+      const userEntityMock: UserEntity = {
+        id: payloadMock.sub,
+        login: 'another_login',
+        email: payloadMock.email,
+      } as any;
+
+      jest
+        .spyOn(userService, 'findOneById')
+        .mockResolvedValueOnce(userEntityMock);
+
+      const resultPromise = jwtStrategy.validate(payloadMock);
+
+      expect(resultPromise).rejects.toThrowError(UnauthorizedException);
+      expect(configService.getOrThrow).toHaveBeenCalledTimes(1);
+      expect(configService.getOrThrow).toHaveBeenCalledWith('JWT_SECRET');
+      expect(userService.findOneById).toHaveBeenCalledTimes(1);
+      expect(userService.findOneById).toHaveBeenCalledWith(payloadMock.sub);
+    });
+
+    it('should throw a unauthorized exception', async () => {
+      const payloadMock: JwtPayloadDto = {
+        sub: randomUUID(),
+        login: 'some_login',
+        email: 'some@login.mail',
+      };
+
+      const userEntityMock: UserEntity = {
+        id: payloadMock.sub,
+        login: payloadMock.login,
+        email: 'some_another@login.mail',
+      } as any;
+
+      jest
+        .spyOn(userService, 'findOneById')
+        .mockResolvedValueOnce(userEntityMock);
+
+      const resultPromise = jwtStrategy.validate(payloadMock);
+
+      expect(resultPromise).rejects.toThrowError(UnauthorizedException);
+      expect(configService.getOrThrow).toHaveBeenCalledTimes(1);
+      expect(configService.getOrThrow).toHaveBeenCalledWith('JWT_SECRET');
+      expect(userService.findOneById).toHaveBeenCalledTimes(1);
+      expect(userService.findOneById).toHaveBeenCalledWith(payloadMock.sub);
     });
   });
 });
--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,12 +1,17 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { JwtPayloadDto } from '../dtos/jwt-payload.dto';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(@Inject(ConfigService) configService: ConfigService) {
+  constructor(
+    @Inject(ConfigService) configService: ConfigService,
+    @Inject(UserService)
+    private readonly userService: UserService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -15,6 +20,19 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: JwtPayloadDto) {
+    const user = await this.userService.findOneById(payload.sub);
+
+    const unauthorizedException =
+      "You're not authorized to perform this action!";
+
+    if (!user) throw new UnauthorizedException(unauthorizedException);
+
+    if (user.email !== payload.email)
+      throw new UnauthorizedException(unauthorizedException);
+
+    if (user.login !== payload.login)
+      throw new UnauthorizedException(unauthorizedException);
+
     return {
       id: payload.sub,
       login: payload.login,
