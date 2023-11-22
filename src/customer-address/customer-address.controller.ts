9cdde3d0b0f94269c671984ba2db3e236ff3c70f
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2023-11-22 09:56:18 -0300

    fix: changed customer-address post controller to use authenticated user
    changed customer-address post controller to use authenticated customer on address record creation

--- src/customer-address/customer-address.controller.ts
+++ src/customer-address/customer-address.controller.ts
@@ -5,14 +5,17 @@ import {
   Inject,
   Param,
   Post,
+  Req,
   Res,
+  UnauthorizedException,
   UseGuards,
 } from '@nestjs/common';
 import { CustomerAddressService } from './customer-address.service';
-import { Response } from 'express';
+import { Request, Response } from 'express';
 import { CreateCustomerAddressControllerDto } from './dtos/create-customer-address-controller.dto';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
+import { request } from 'http';
 
 @ApiTags('Customer Address')
 @Controller('customer-address')
@@ -24,20 +27,23 @@ export class CustomerAddressController {
     private readonly customerAddressService: CustomerAddressService,
   ) {}
 
-  @Post(':customerId')
+  @Post()
   async create(
-    @Param('customerId') customerId: string,
     @Body() createCustomerAddressDto: CreateCustomerAddressControllerDto,
+    @Req() request: Request,
     @Res() response: Response,
   ) {
+    const user = request.user as any;
+
+    if(!user) throw new UnauthorizedException('You\'re not allowed to perform this action')
     const customerAddresId = await this.customerAddressService.create({
       ...createCustomerAddressDto,
       customer: {
-        id: customerId,
+        id: user.id,
       },
     });
 
-    const getUrl = `customer-address/${customerId}/${customerAddresId}`;
+    const getUrl = `customer-address/${user.id}/${customerAddresId}`;
 
     response.header('location', getUrl).status(HttpStatus.CREATED).send();
 
