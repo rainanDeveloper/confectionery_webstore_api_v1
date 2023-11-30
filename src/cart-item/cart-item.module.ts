4be18b050d9d99361d8f758a979bda1c004057f7
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2023-11-30 17:00:25 -0300

    feat: Added cart-item service to application

--- src/cart-item/cart-item.module.ts
+++ src/cart-item/cart-item.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CartItemEntity } from './entities/cart-item.entity';
+import { CartItemService } from './cart-item.service';
 
 @Module({
   imports: [ConfigModule, TypeOrmModule.forFeature([CartItemEntity])],
+  providers: [CartItemService],
 })
 export class CartItemModule {}
