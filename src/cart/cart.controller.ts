c6d94111c944c7fb970a09f1adaa38011f6c53e0
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2023-12-12 17:16:46 -0300

    feat: Added create cart controller on application

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,7 +1,29 @@
-import { Controller, Inject } from '@nestjs/common';
+import { Body, Controller, Inject, Req } from '@nestjs/common';
 import { CartService } from './cart.service';
+import { CreateCartControllerDto } from './dtos/create-cart-controller.dto';
+import { Request } from 'express';
+import { CreateCartServiceDto } from './dtos/create-cart-service.dto';
 
 @Controller('cart')
 export class CartController {
   constructor(@Inject(CartService) private readonly cartService) {}
+
+  async create(
+    @Req() request: Request,
+    @Body() createCartControllerDto: CreateCartControllerDto,
+  ) {
+    const user = request.user as any;
+
+    const createCartServiceDto: CreateCartServiceDto = {
+      itens: createCartControllerDto.itens,
+    };
+
+    if (user) {
+      createCartServiceDto.customer = {
+        id: user.id,
+      };
+    }
+
+    return await this.cartService.create(createCartServiceDto);
+  }
 }
