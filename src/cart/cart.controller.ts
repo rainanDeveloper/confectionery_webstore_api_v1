eb5795607125080b5064d6c3a4a5c31c441b41e0
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2023-12-16 00:11:31 -0300

    feat: Added findOne cart controller

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,10 +1,20 @@
-import { Body, Controller, Inject, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Post,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { CartService } from './cart.service';
 import { CreateCartControllerDto } from './dtos/create-cart-controller.dto';
 import { Request } from 'express';
 import { CreateCartServiceDto } from './dtos/create-cart-service.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { OptionalJwtCustomerGuard } from 'src/customer-auth/guards/optional-jwt-customer.guard';
+import { CartEntity } from './entities/cart.entity';
 
 @Controller('cart')
 @ApiTags('Cart')
@@ -32,5 +42,19 @@ export class CartController {
     const cartId = await this.cartService.create(createCartServiceDto);
 
     return { id: cartId };
+  }
+
+  @Get()
+  async findOne(
+    @Req() request: Request,
+    @Query('id') id: string,
+  ): Promise<CartEntity> {
+    const user = request.user as any;
+
+    if (user) {
+      return this.cartService.findAnyOpenForCustomer(user.id, true);
+    }
+
+    return this.cartService.findOne(id, true);
   }
 }
