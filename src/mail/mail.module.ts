bd1215210bead38cdf8e9acf910f1a4f35009148
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2024-01-07 02:05:18 -0300

    feat: Configured mail module

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,4 +1,32 @@
 import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigService } from '@nestjs/config';
+import { join } from 'path';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 
-@Module({})
+@Module({
+  imports: [
+    MailerModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        transport: {
+          host: configService.getOrThrow('SMTP_HOST'),
+          port: parseInt(configService.getOrThrow('SMTP_PORT')),
+          secure: Boolean(configService.getOrThrow('SMTP_SECURE')),
+          auth: {
+            user: configService.getOrThrow('SMTP_EMAIL'),
+            pass: configService.getOrThrow('SMTP_PASS'),
+          },
+        },
+        template: {
+          dir: join(process.cwd(), 'src', 'mail', 'templates'),
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
 export class MailModule {}
