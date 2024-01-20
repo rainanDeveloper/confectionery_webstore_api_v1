3d047b2e12931fb650d8625947bb8bee0c39cdad
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2024-01-20 20:52:54 -0300

    feat: added method create on order service

--- src/order/order.service.spec.ts
+++ src/order/order.service.spec.ts
@@ -1,18 +1,63 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrderService } from './order.service';
+import { Repository } from 'typeorm';
+import { OrderEntity } from './entities/order.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { randomUUID } from 'crypto';
+import { OrderStatus } from './enum/order-status.enum';
 
 describe('OrderService', () => {
   let orderService: OrderService;
+  let orderRepository: Repository<OrderEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OrderService],
+      providers: [
+        OrderService,
+        {
+          provide: getRepositoryToken(OrderEntity),
+          useValue: {
+            create: jest.fn(),
+            save: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     orderService = module.get<OrderService>(OrderService);
+    orderRepository = module.get<Repository<OrderEntity>>(
+      getRepositoryToken(OrderEntity),
+    );
   });
 
   it('should be defined', () => {
     expect(orderService).toBeDefined();
+    expect(orderRepository).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a order successfully', async () => {
+      const nowMock = new Date();
+      const orderMock: OrderEntity = {
+        id: randomUUID(),
+        itens: [],
+        total: 0,
+        status: OrderStatus.OPEN,
+        createdAt: nowMock,
+        updatedAt: nowMock,
+      } as OrderEntity;
+
+      jest.spyOn(orderRepository, 'create').mockReturnValueOnce(orderMock);
+
+      const result = await orderService.create();
+
+      expect(result).toStrictEqual(orderMock);
+      expect(orderRepository.create).toHaveBeenCalledTimes(1);
+      expect(orderRepository.create).toHaveBeenCalledWith({
+        status: OrderStatus.OPEN,
+      });
+      expect(orderRepository.save).toHaveBeenCalledTimes(1);
+      expect(orderRepository.save).toHaveBeenCalledWith(orderMock);
+    });
   });
 });
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { OrderEntity } from './entities/order.entity';
+import { Repository } from 'typeorm';
+import { OrderStatus } from './enum/order-status.enum';
 
 @Injectable()
-export class OrderService {}
+export class OrderService {
+  constructor(
+    @InjectRepository(OrderEntity)
+    private readonly orderRepository: Repository<OrderEntity>,
+  ) {}
+
+  async create(): Promise<OrderEntity> {
+    const createdOrder = this.orderRepository.create({
+      status: OrderStatus.OPEN,
+    });
+    await this.orderRepository.save(createdOrder);
+    return createdOrder;
+  }
+}
